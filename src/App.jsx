69df19e0f084f33e3466ee3745f42db1081a5712
Valentina Lujan Aleman <valentinaaleman07@gmial.com>
2023-08-29 07:19:16 -0300

    hago cambios

--- src/App.jsx
+++ src/App.jsx
@@ -1,4 +1,4 @@
-import { useState } from "react";
+import React from 'react';
 import NavbarComponent from './components/NavbarComponent';
 
 function App() {
--- src/components/NavbarComponent.jsx
+++ src/components/NavbarComponent.jsx
@@ -1,17 +1,17 @@
-import { useState } from "react";
+import React from 'react';
 
 const NavbarComponent = () => {
   return ( 
-<nav class="navbar navbar-expand-lg bg-body-tertiary">
-  <div class="container-fluid">       
-  <li class="nav-item dropdown"> 
-  <a class="nav-link dropdown-toggle" href="#" role="button" data-bs-toggle="dropdown" aria-expanded="false">
+<nav className="navbar navbar-expand-lg bg-body-tertiary">
+  <div className="container-fluid">       
+  <li className="nav-item dropdown"> 
+  <a className="nav-link dropdown-toggle" href="#" role="button" data-bs-toggle="dropdown" aria-expanded="false">
   Menú
   </a>
   <ul class="dropdown-menu">
-    <li><a class="dropdown-item" href="#">Menú</a></li>
-    <li><a class="dropdown-item" href="#">Tienda</a></li>
-    <li><a class="dropdown-item" href="#">Redes sociales y contactos</a></li>
+    <li><a className="dropdown-item" href="#">Menú</a></li>
+    <li><a className="dropdown-item" href="#">Tienda</a></li>
+    <li><a className="dropdown-item" href="#">Redes sociales y contactos</a></li>
   </ul>
 </li>
 </div>
